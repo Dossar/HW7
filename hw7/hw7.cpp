4dbef2c525fad515a9eb236feb091ec4e26db860
Roy Van Liew <vanliewroy@gmail.com>
2014-04-09 11:17:43 -0400

    Working deposit and withdraw for BankAccount

--- hw7/hw7.cpp
+++ hw7/hw7.cpp
@@ -2,7 +2,7 @@
  * File:   hw7.cpp
  * Author: Roy Van Liew and Saqib Zahid
  *
- * Last modified on April 8, 2014, 5:28 PM
+ * Last modified on April 9, 2014, 11:17 AM
  */
 
 #include <iostream>
@@ -55,7 +55,6 @@ class MoneyMarketAccount : BankAccount{
             int withdraw( double amount ); // Overridden withdraw for MoneyMarketAccount
     protected:
             int numOfWithdr; // Number of withdrawals. If zero, withdrawal is free.
-            double fee; // Withdrawal fee if account has already done a withdrawal.
 
 };
 
@@ -87,6 +86,37 @@ class CDAccount : BankAccount{
 /*              FUNCTION STUFF FOR CLASSES               */
 /*********************************************************/
 
+// Add money to BankAccount.
+void BankAccount::deposit( double amount ){
+    
+    // If the amount is negative, do not deposit.
+    if( amount < 0 )
+        cout << "Cannot deposit negative money." << endl;    
+    else
+        balance += amount;
+    
+}
+
+// Withdraw money from BankAccount.
+// Return 0 if balance is enough.
+// Return 1 for insufficient funds or error case.
+int BankAccount::withdraw( double amount ){
+    
+    // If withdraw amount is negative, it is an error case.
+    if( amount < 0 ){
+        cout << "Cannot withdraw negative money." << endl;
+        return 1;
+    }
+    // If withdraw amount is higher than current balance, it is an error case.
+    if( amount > balance ){
+        cout << "Insufficient funds." << endl;
+        return 1;
+    }
+    // Otherwise, amount to withdraw is legitimate.
+    balance -= amount;
+    
+}
+
 // Overloaded output operator
 ostream& operator <<(ostream& out, const BankAccount& Account){
     
@@ -109,8 +139,14 @@ istream& operator >>(istream& in, BankAccount& Account){
 
 int main() {
     
+    // Create a bank account and test the input and output operators.
     BankAccount account1;
     cin >> account1;
+    // Testing deposit and withdraw
+    account1.deposit(-2);
+    account1.deposit(100.25);
+    account1.withdraw(-1);
+    account1.withdraw(300.50);
     cout << account1;
 
     return 0;

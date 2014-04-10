7b35538a1bd59a2a6cce177eef4a9170cee8df7c
Roy Van Liew <vanliewroy@gmail.com>
2014-04-10 19:56:41 -0400

    Wrote withdraw for all the functions

--- hw7/hw7.cpp
+++ hw7/hw7.cpp
@@ -2,7 +2,7 @@
  * File:   hw7.cpp
  * Author: Roy Van Liew and Saqib Zahid
  *
- * Last modified on April 9, 2014, 11:17 AM
+ * Last modified on April 10, 2014, 7:55 PM
  */
 
 #include <iostream>
@@ -22,19 +22,17 @@ using namespace std;
 class BankAccount{
     
     public:
-//        BankAccount(); // Default Constructor
-//        BankAccount(const BankAccount& Account); // Copy Constructor
-//        ~BankAccount(); // Destructor
+        BankAccount(); // Default Constructor
+        BankAccount(string accName, double amount); // Constructor with parameters
+        
+        /**** Methods and overloaded output operator for BankAccount ****/
         void transfer( BankAccount& To , double amount );
         void deposit( double amount ); // Put money into account
-        int withdraw( double amount ); // int returned: 0 for "ok", 1 for "insufficient funds".
-        
-        /**** Overloaded operators ****/
-        friend ostream& operator <<(ostream& out, const BankAccount& Account);
-        friend istream& operator >>(istream& in, BankAccount& Account);
+        int withdraw( double amount ); // int returned: 0 for "ok", 1 for "insufficient funds".               
+        friend ostream& operator <<( ostream& out, const BankAccount& Account );
 
         /**** Accessor functions for account name and balance. ****/
-        string getName(){
+        string getName(){ 
             return name;
         }
         double getBalance(){
@@ -48,28 +46,72 @@ class BankAccount{
     
 };
 
-// Inherited from BankAccount, has withdrawal fees.
+// Default constructor for BankAccount, prompt use to enter in data
+BankAccount::BankAccount(){
+    
+    cout << "Please enter the owner of this account: ";
+    cin >> name;
+    cout << "Please enter the balance of this account: ";
+    cin >> balance;
+    
+}
+
+// Constructor with parameters for BankAccount.
+BankAccount::BankAccount( string accName, double amount ){
+    
+    name = accName;
+    balance = amount;
+    
+}
+
+/* MoneyMarketAccount is derived from BankAccount.
+ * withdraw function is overridden so there is a withdrawal fee.
+ * There is also a member variable keeping track of what's been withdrawn. */
 class MoneyMarketAccount : BankAccount{
 
     public:
+            // Default constructor for MoneyMarketAccount, prompt use to enter in data
+            MoneyMarketAccount() : BankAccount()
+            {
+                    cout << "Please enter the number of withdrawals that have occurred: ";
+                    cin >> numOfWithdr;    
+            }
+            // Constructor with parameters for MoneyMarketAccount.
+            MoneyMarketAccount(string accName, double amount, int withDr);
+
             int withdraw( double amount ); // Overridden withdraw for MoneyMarketAccount
+            // friend functions are not inherited.
+            friend ostream& operator <<( ostream& out, const MoneyMarketAccount& Account );
     protected:
             int numOfWithdr; // Number of withdrawals. If zero, withdrawal is free.
 
 };
 
-// Inherited from BankAccount, has interest rate.
+// Constructor with parameters for MoneyMarketAccount.
+// Extra parameter is number of withdrawals.
+MoneyMarketAccount::MoneyMarketAccount(string accName, double amount, int withDr) {
+    
+    name = accName;
+    balance = amount;
+    numOfWithdr = withDr;
+    
+}
+
+/* CDAccount is derived from BankAccount.
+ * withdraw function is overridden so a penalty on interest rate is incurred. */
 class CDAccount : BankAccount{
 
     public:
             int withdraw( double amount ); // Overridden withdraw for CDAccount
+            // friend functions are not inherited.
+            friend ostream& operator <<( ostream& out, const CDAccount& Account );            
     protected:
             double interestRate;
 
 };
 
 /*********************************************************/
-/*              FUNCTION STUFF FOR CLASSES               */
+/*              METHODS FOR BANKACCOUNT                  */
 /*********************************************************/
 
 // Add money to BankAccount.
@@ -111,55 +153,118 @@ void BankAccount::transfer( BankAccount& To , double amount ) {
 
 	int status; // Determines if the account withdrawn from has sufficient funds or amount is valid.
 	status = withdraw( amount ); // Withdraw from current account.
+        // If status is zero, amount is legal.
 	if( status == 0 ) {
 		To.deposit( amount ); // Deposit to an account passed in the parameter
-		cout << "Transferred $" << amount << endl;
+		cout << "Transferred $" << amount << " from " << name << " to " << To.getName() << endl;
 	}
+        // Otherwise, amount was either negative or too much.
 	if( status == 1 )
 		cout << "Transfer unsuccessful." << endl;
 
 }
 
-// Overloaded output operator
-ostream& operator <<(ostream& out, const BankAccount& Account){
+// Overloaded output operator for BankAccount
+ostream& operator <<( ostream& out, BankAccount& Account ){
     
-    out << "Type: " << typeid(Account).name() << endl; // Print out what type of bank account it is
-    out << "Owner: " << Account.name << endl;
-    out << "Balance: $" << Account.balance << endl;
-    
-}
-
-// Overloaded input operator
-istream& operator >>(istream& in, BankAccount& Account){
-    
-    cout << "Input owner's name: ";
-    in >> Account.name;
-    cout << "Input balance: ";
-    in >> Account.balance;
-    cout << endl;
+    out << "Type: Bank Account" << endl; // Print out what type of bank account it is
+    out << "Owner: " << Account.getName() << endl;
+    out << "Balance: $" << Account.getBalance() << endl;
     
 }
 
+/*********************************************************/
+/*              METHODS FOR MONEYMARKETACCOUNT           */
+/*********************************************************/
+
+// Withdraw money from MoneyMarketAccount.
+// Return 0 if balance is enough.
+// Return 1 for insufficient funds or error case.
+// Incur $1.50 penalty if withdrawals is greater than zero.
+int MoneyMarketAccount::withdraw( double amount ){
+    
+    // If withdraw amount is negative, it is an error case.
+    if( amount < 0 ){
+        cout << "Cannot withdraw negative money." << endl;
+        return 1;
+    }
+    // If withdraw amount is higher than current balance, it is an error case.
+    if( amount > balance ){
+        cout << "Insufficient funds." << endl;
+        return 1;
+    }
+    // Otherwise, amount to withdraw is legitimate.
+    // User gets one free withdrawal. If there has been one used already, incur $1.50 penalty.
+    if( numOfWithdr == 0 )
+        balance -= amount;
+    if( numOfWithdr > 0 )
+        balance -= ( amount + 1.50 );
+    return 0; // Returning 0 means the withdrawal was successful.
+    
+}
+
+//// Overloaded output operator for MoneyMarketAccount
+//ostream& operator <<( ostream& out, MoneyMarketAccount& Account ){
+//    
+//    out << "Type: Bank Account" << endl; // Print out what type of bank account it is
+//    out << "Owner: " << Account.getName() << endl;
+//    out << "Balance: $" << Account.getBalance() << endl;
+//    
+//}
+
+/*********************************************************/
+/*              METHODS FOR CDACCOUNT                    */
+/*********************************************************/
+
+// Withdraw money from MoneyMarketAccount.
+// Return 0 if balance is enough.
+// Return 1 for insufficient funds or error case.
+// Incur 25% penalty on interest rate for any valid withdrawal amount made.
+int CDAccount::withdraw( double amount ){
+    
+    // If withdraw amount is negative, it is an error case.
+    if( amount < 0 ){
+        cout << "Cannot withdraw negative money." << endl;
+        return 1;
+    }
+    // If withdraw amount is higher than current balance, it is an error case.
+    if( amount > balance ){
+        cout << "Insufficient funds." << endl;
+        return 1;
+    }
+    // Otherwise, amount to withdraw is legitimate.
+    // Incur the 25% penalty to the interest rate and also subtract it from balance.
+    double penalty = (interestRate*0.25);
+    interestRate -= penalty; // Subtract 25% from interest as a penalty
+    balance -= ( amount + penalty );
+    return 0; // Returning 0 means the withdrawal was successful.
+    
+}
+
+
+
+// Start of main. Here we create three accounts each of different types.
 int main() {
     
     // Create a bank account and test the input and output operators.
     BankAccount account1;
-    cin >> account1;
-    
+    BankAccount account2("Luis",200);
+//    
 //    // Testing deposit and withdraw
 //    account1.deposit(-2);
 //    account1.deposit(100.25);
 //    account1.withdraw(-1);
-//    account1.withdraw(300.50);
-    
+//    account1.withdraw(300.50);   
     cout << account1;
+    cout << account2;
     
-    // Create a copy of account 1. Test transfer.
-    BankAccount account2 = account1;
+    // Test transfer.
     account2.transfer(account1,50);
-    account2.transfer(account1,-1);
+    //account2.transfer(account1,-1);
     cout << account2;
     cout << account1;
+    
+//    MoneyMarketAccount m1;
 
     return 0;
 }
